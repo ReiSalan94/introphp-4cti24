73da8975cbcb169e01f61b1dfa237fc4020fc403
Prof Alan Reis <98103226+ReiSalan94@users.noreply.github.com>
2024-04-12 21:06:38 -0300

    Add files via upload

---
+++ 3_lacos_repeticao/4_foreach.php
@@ -0,0 +1,24 @@
+<?php
+/*estrutura de repetição para trabalhar com arrays */
+$mendes = array(
+    "Xandão é o cara!",
+    " xanxan, ",
+    " bora BGS! "
+);
+
+foreach ($mendes as $view) {
+    echo $view;
+}
+?>
+<hr>
+<?php
+$meses = array(
+    'Janeiro' => 1,
+    'Fevereiro' => 2,
+    'Março' => 3,
+    'Abril' => 4,
+);
+foreach($meses as $chave => $valor){
+    echo ' O mês '. $chave . ' é ' . $valor .' <br>';
+}
+?>
